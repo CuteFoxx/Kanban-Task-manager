cf94b089510e03d05c8aabe3d4a6ebce73f67f38
Alice <alicepolishchuk1@gmail.com>
2025-09-14 16:10:46 +0300

    Add cors origin env

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [/^http:\/\/localhost:\d+$/],
+    origin: process.env.ORIGIN ?? [/^http:\/\/localhost:\d+$/],
     credentials: true,
   });
 

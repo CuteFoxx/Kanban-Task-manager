b9aff3210973859ddc661d4f57079c84462e0bea
Alice <alicepolishchuk1@gmail.com>
2025-09-14 15:53:35 +0300

    exported api url to env

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 createRoot(document.body).render(
   <StrictMode>
